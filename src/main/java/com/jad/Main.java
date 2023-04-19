cb3daabc19da55be5eabf031aa2937d4aa6a36e9
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2023-04-19 18:15:12 +0200

    Step 2 done. All show creations must go through the ShowFactory class.

--- src/main/java/com/jad/Main.java
+++ src/main/java/com/jad/Main.java
@@ -12,23 +12,23 @@ public final class Main {
         final ISpectator jad = new Spectator("Jean-Aymeric", "Diet");
 
         final List<IShow> shows = new ArrayList<>();
-        shows.add(new MovieShow("Titre du film",
+        shows.add(ShowFactory.makeMovieShow("Titre du film",
                 "Description du film",
                 "Nom du réalisateur",
                 "2023",
                 MovieType.SCIENCE_FICTION));
-        shows.add(new TheaterShow("Titre du spectacle de théâtre",
+        shows.add(ShowFactory.makeTheaterShow("Titre du spectacle de théâtre",
                 "Description du spectacle de théâtre",
                 "Nom du metteur en scène",
                 "Nom de l'acteur 1",
                 "Nom de l'acteur 2",
                 "Nom de l'acteur 3"));
-        shows.add(new StreetShow("Titre du spectacle de rue",
+        shows.add(ShowFactory.makeStreetShow("Titre du spectacle de rue",
                 "Description du spectacle de rue",
                 "Nom de l'artiste 1",
                 "Nom de l'artiste 2",
                 "Nom de l'artiste 3"));
-        shows.add(new ConcertShow("Titre du concert",
+        shows.add(ShowFactory.makeConcertShow("Titre du concert",
                 "Description du concert",
                 "Nom de l'artiste ou du groupe"));
 
